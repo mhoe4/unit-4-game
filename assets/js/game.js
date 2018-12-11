61afa999dcda24e0f5e710572252c00f93578530
mhoef <mhoefer1@gmail.com>
2018-12-11 16:17:14 -0500

    fancybox

--- assets/js/game.js
+++ assets/js/game.js
@@ -16,17 +16,25 @@ $(document).ready(function() {
   // Theme Button
   $(".my-sound-btn").on("click", function() {
     if ($(".my-sound-btn").text().includes("On") ) {
-      $(".my-sound-btn").css('background-color', 'red');
+      $(".my-sound-btn").css('background-color', 'rgb(255,0,0)');
       $(".my-sound-btn").html('<span class="fa fa-music"></span> Game Play Sound Off');
     } else {
-      $(".my-sound-btn").css('background-color', 'green');
+      $(".my-sound-btn").css('background-color', 'rgb(90, 255, 13)');
       $(".my-sound-btn").html('<span class="fa fa-music"></span> Game Play Sound On');
     }
   });
 
-  
 
+});
 
+$("#game-info").on("click", function() {
+
+  $.fancybox.open('<div class="message text-center"><h2>How to Play!</h2>\
+  <p>A random number between 19-120 is generated at the beginning of the game and is displayed in the top left. This is your target number.</p>\
+  <p>There are four crystals, each assigned a random value between 1-12. Clicking on a crystal will add the value of that crystal to your score, which is displayed at the bottom of the screen.</p>\
+  <p>The user wins the game by matching the value of their score to the target number. The user loses if they go over the target number.</p>\
+  <p>When the user either wins or loses a game, a new target number is generated and new values are assigned to each crystal. Wins and loses are tracked along the way!</p>\
+  </div>');
 });
 
 $(".gem").on("click", function() {
